a3726b5c8ee023e7acb1a6756ad6c04c6223da6f
ConnCaster <example@example.com>
2022-06-13 23:06:45 +0300

    add TgBot library

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "fs_operations.h"
 #include <tgbot/tgbot.h>
+#include <chrono>
 
 int main(){
 
@@ -8,7 +9,35 @@ int main(){
         std::cerr << "[ERROR] token reading error";
         return 1;
     }
-    std::cout << "[TOKEN]\t" << token;
-//    TgBot::Bot bot(token);
+    std::cout << "[TOKEN]\t" << token << std::endl;
+    TgBot::Bot bot(token);
+
+    bot.getEvents().onCommand("hello", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi! " + message->chat->username);
+    });
+
+    bot.getEvents().onCommand("date", [&bot](TgBot::Message::Ptr message) {
+        auto time_now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        std::string time{ctime(&time_now)};
+        bot.getApi().sendMessage(message->chat->id, time);
+    });
+
+//    bot.getEvents().onCommand("sticker", [&bot](TgBot::Message::Ptr message) {
+//        auto time_now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+//        std::string time{ctime(&time_now)};
+//        bot.getApi().;
+//    });
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+
     return 0;
 }
