2d5f7675ec6b3fd0e1d175e1aa22bce68e311ace
ConnCaster <example@example.com>
2022-07-05 20:29:11 +0300

    add stickers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "fs_operations.h"
 #include <tgbot/tgbot.h>
 #include <chrono>
+#include <random>
 
 int main(){
 
@@ -22,11 +23,15 @@ int main(){
         bot.getApi().sendMessage(message->chat->id, time);
     });
 
-//    bot.getEvents().onCommand("sticker", [&bot](TgBot::Message::Ptr message) {
-//        auto time_now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-//        std::string time{ctime(&time_now)};
-//        bot.getApi().;
-//    });
+    bot.getEvents().onCommand("hasbik", [&bot](TgBot::Message::Ptr message) {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(1, 120);
+        std::cout << dist(mt) << std::endl;
+        std::string sticker_path = "/home/user/dir/programming/C++/TgBot/media/hasbullaking/webp/hasbik_" + std::to_string(dist(mt)) + ".webp";
+        std::cout << sticker_path << std::endl;
+        bot.getApi().sendSticker(message->chat->id, TgBot::InputFile::fromFile(sticker_path, "image/webp"));
+    });
 
     try {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
